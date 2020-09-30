a9547bad1aea0417231ca41e01ff1a114f57383c
Nome Sobrenome <seu_email@email.com>
2020-09-30 15:12:38 -0300

    subindo arquivos

---
+++ semana3/aula09/exercicio.js
@@ -0,0 +1,10 @@
+let nome
+console.log(typeof nome)
+
+let idade
+console.log(typeof idade)
+
+/**  o tipo "undefined" foi impresso porque as duas variaveis não possuem valor definido 
+ */ 
+
+ console.log("olá, bom dia!. qual seu nome? ", nome, "e sua idade ?", idade)
